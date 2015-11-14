5e106e356011b27e6fb8bdf22f48e3d270ecfe1b
Paul Williams <paul@paulswilliams.me.uk>
2015-11-14 13:45:11 +0000

    Updated project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler-solver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My attempt at Project Euler with Clojure"
+  :url "https://github.com/paulspencerwilliams/euler-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
